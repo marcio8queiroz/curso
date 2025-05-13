7c0da1dce6130dfc1c62dceff4f4ab18eaf4480d
marcio8queiroz <marcio8queiroz@hotmail.com>
2025-05-13 10:10:09 -0400

    criando componente navbar e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ExemploComponenteComponent } from './_components/exemplo-componente/exemplo-componente.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './_components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ExemploComponenteComponent
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,

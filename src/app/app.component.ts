c3f9eadd45c72f0a8adeb9cc088d11ebb6c0810c
marcio8queiroz <marcio8queiroz@hotmail.com>
2025-05-15 09:13:57 -0400

    NgDoCheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,4 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
- exibeHome: boolean = true;
-  destruir(){
-    this.exibeHome = false;
-  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnDestroy {
-  ngOnDestroy(): void {
-      console.log('Destruiu');
+export class HomeComponent implements DoCheck {
+
+  num: number = 1;
+
+  ngDoCheck(): void {
+      console.log('Uma mudança foi feita');
+  }
+
+  adiciona1(){
+   this.num++;
   }
 }

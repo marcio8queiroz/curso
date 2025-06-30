33368dccf1f7462e7a5b15d6fa4f6dd142f50733
marcio8queiroz <marcio8queiroz@hotmail.com>
2025-06-30 11:46:50 -0400

    query params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { UserComponent } from './pages/user/user.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users/list', component: UsersComponent},
-  {path: 'users/:username', component: UserComponent},
+  {path: 'users/details', component: UserComponent},
 ];
 
 @NgModule({
